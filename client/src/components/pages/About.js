461408a24bfc05043fc22bae6770c5f6f78bb70a
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-07-20 15:21:17 +0200

    some filing structure in place

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+	return (
+		<div>
+			<h1>About this web application</h1>
+			<p>
+				This is a fullstack application which is connected to a server as a
+				backend.
+			</p>
+			<p>
+				<strong>Version:</strong>1.0.0
+			</p>
+		</div>
+	);
+}
+
+export default About;
