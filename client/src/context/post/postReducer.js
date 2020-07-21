cac1f1a97a76aa889cedf20dd1b38b3950bb8318
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-07-21 13:27:53 +0200

    user, post context and state

---
+++ client/src/context/post/postReducer.js
@@ -0,0 +1,75 @@
+import {
+	GET_POSTS,
+	ADD_POST,
+	DELETE_POST,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	FILTER_POSTS,
+	POST_ERROR,
+	CLEAR_ERRORS,
+	CLEAR_POSTS,
+	CLEAR_FILTER,
+} from "../types";
+
+export default (state, action) => {
+	switch (action.type) {
+		case GET_POSTS:
+			return {
+				...state,
+				posts: action.payload,
+				loading: false,
+			};
+		case ADD_POST:
+			return {
+				...state,
+				posts: [actions.payload, ...state.posts],
+				loading: false,
+			};
+		case DELETE_POST:
+			return {
+				...state,
+				posts: state.posts.map((post) =>
+					post._id === action.payload._id ? action.payload : post
+				),
+				loading: false,
+			};
+		case CLEAR_POSTS:
+			return {
+				...state,
+				posts: null,
+				filtered: null,
+				error: null,
+				current: null,
+			};
+		case SET_CURRENT:
+			return {
+				...state,
+				current: action.payload,
+			};
+		case CLEAR_CURRENT:
+			return {
+				...state,
+				current: null,
+			};
+		case FILTER_POSTS:
+			return {
+				...state,
+				filtered: state.post.filter((post) => {
+					const regex = new RegExp(`${action.payload}`, "gi");
+					return post.type.match(regex);
+				}),
+			};
+		case CLEAR_FILTER:
+			return {
+				...state,
+				filtered: null,
+			};
+		case POST_ERROR:
+			return {
+				...state,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
