57c4d46837b341811e97b81b72f6d3316bb139ec
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-07-29 09:10:23 +0200

    post object fiddling

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
 const Post = require("../models/Post");
@@ -7,7 +9,7 @@ const Post = require("../models/Post");
 // @route   GET api/posts
 // @desc    Get all posts
 // @access  Public
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
 	try {
 		const posts = await Post.find({ user: req.user.id }).sort({
 			date: -1,
@@ -22,33 +24,42 @@ router.get("/", async (req, res) => {
 // @route   POST api/posts
 // @desc    Add a new post
 // @access  Public
-router.post("/", async (req, res) => {
-	const { posts, type } = req.body;
+router.post(
+	"/",
+	[check("post", "Post is required").not().isEmpty()],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-	try {
-		const newPost = new Post({
-			posts,
-			type,
-			user: req.user.id,
-		});
+		const { post, type } = req.body;
 
-		const post = await newPost.save();
+		try {
+			const newPost = new Post({
+				post,
+				type,
+				user: req.user.id,
+			});
 
-		res.json({ type, posts });
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send("Server Error");
+			const post = await newPost.save();
+
+			res.json({ type, post });
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server Error");
+		}
 	}
-});
+);
 
 // @route   PUT api/posts/:id
 // @desc    Update post
 // @access  Public
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
 	const { post, type } = req.body;
-	
+
 	//Post object
-	const postFields = {}
+	const postFields = {};
 	if ("") postFields.post = post;
 	if (type) postFields.type = type;
 
@@ -57,7 +68,11 @@ router.put("/:id", async (req, res) => {
 
 		if (!post) return res.status(404).json({ msg: "Post not found" });
 
-		
+		// Make sure user owns post
+		if (post.user.toString() !== req.user.id) {
+			return res.status(404).json({ msg: "Not Authorized " });
+		}
+
 		post = await Post.findByIdAndUpdate(
 			req.params.id,
 			{ $set: postFields },
@@ -78,16 +93,15 @@ router.delete("/:id", async (req, res) => {
 	try {
 		let post = await Post.findById(req.params.id);
 
-		if (!post) return res.status(404).json({ msg: "Post not found " })
-		
+		if (!post) return res.status(404).json({ msg: "Post not found " });
+
 		await Post.findByIdAndRemove(req.params.id);
 
-		res.json({msg:"Post Removed "})
+		res.json({ msg: "Post Removed " });
 	} catch (err) {
 		console.error(err.message);
-		res.status(500).send("Server Error")
-		
-	};
+		res.status(500).send("Server Error");
+	}
 });
 
 module.exports = router;
