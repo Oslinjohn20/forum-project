60c89e987bf249ae3c64b68bc340063112348992
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-08-02 14:59:24 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require("path")
 const app = express();
 
 // Connect to database
@@ -18,10 +18,14 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 
-// Getting the data from the database 
-app.get("/api/posts", (req, res) => {
-    const posts = data.posts
-    res.send(posts)
-})
+//Serve static assests in production
+if (process.env.NODE_ENV === "production") {
+    // Static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req,res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+}
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
