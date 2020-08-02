b712017cada297ce23c467e618153104c1811ada
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-08-02 12:35:42 +0200

    errors are persistant

---
+++ client/build/precache-manifest.e66396c3141d7fca1d8f3c2c8a104d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b677c23d2d1bbd935499b5417bdcce37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0ff82aec10d6907773",
+    "url": "/static/css/main.ca1d50a0.chunk.css"
+  },
+  {
+    "revision": "464f9d118356fd46b681",
+    "url": "/static/js/2.a631c6a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a631c6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0ff82aec10d6907773",
+    "url": "/static/js/main.0e5f1fae.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
